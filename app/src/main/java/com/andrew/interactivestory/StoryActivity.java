e67ab33bcd0227ceceb23e8677441b01fad9cf41
andrewhamilton0 <acjts425@gmail.com>
2022-04-05 20:48:19 -0400

    Adds StoryActivity class. Adds new color pallet and adds name variable and returns it to StoryActivity.

---
+++ app/src/main/java/com/andrew/interactivestory/StoryActivity.java
@@ -0,0 +1,21 @@
+package com.andrew.interactivestory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(getString(R.string.key_name));
+        if (name == null || name.isEmpty()){
+            name = "Friend";
+        }
+    }
+
+}
